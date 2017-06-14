c78c556b16978a91734c694afcedd701c82f5c2d
waws80 <523928638@qq.com>
2017-06-14 08:56:16 +0800

    添加流程图

--- antlib/src/main/java/pw/androidthanatos/ant/controller/HttpListener.kt
+++ antlib/src/main/java/pw/androidthanatos/ant/controller/HttpListener.kt
@@ -8,6 +8,15 @@ import java.io.InputStream
  */
 interface HttpListener {
 
+    /**
+     * 网络获取数据成功回调
+     * @param dataLength 数据的总长度 通过响应头获取
+     * @param cacheText 缓存的文本，没有传空字符串
+     * @param stream 网络获取的io流
+     * @param cache 是否启用cache
+     * @param responseCache 缓存对象
+     * @param tag url标识   使用系统方法 eg： getCacheKey(chain.url,chain.method,chain.headers)
+     */
     fun success(dataLength: Long,cacheText: String ,stream: InputStream,cache: Boolean, responseCache: ResponseCache,tag: String)
 
     fun getCache(text: String)
